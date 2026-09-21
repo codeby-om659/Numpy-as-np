a0d7f2a8078748131fd91b36200f530b609fe154
Om verma <sureshverma67895@gmail.com>
2026-09-21 06:58:07 +0530

    Add NumPy array creation examples in makeArrayByFun.py
    Demonstrate various NumPy array creation methods including full, ones, arange, and identity matrix.

---
+++ makeArrayByFun.py
@@ -0,0 +1,12 @@
+import numpy as np
+filled_arr=np.full((2,3),7)#full({shape,size},value)
+print(filled_arr)
+#ones()is me shape fill kar do utne ones aa jayenge
+ones_arr=np.ones((2,3))
+print(ones_arr)
+#arange()is me start,stop step dalna hai bas 
+arr=np.arange(1,10,2)
+print(arr)
+#creting identity matrix
+id_matrix=np.eye(4)
+print(id_matrix)
