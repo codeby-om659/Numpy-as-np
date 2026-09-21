be882ce81566a5432129205f2f79429b0a1aec1f
Om verma <sureshverma67895@gmail.com>
2026-09-21 07:07:46 +0530

    Implement array splitting using numpy
    Add functionality to split an array into sub-arrays using numpy.

---
+++ split.py
@@ -0,0 +1,9 @@
+#main array ko sub array mai convert karna
+#np.split()=equally parts mai divide karega
+"""
+np.hspliy(horizontal mai divide)
+np.vsplit() vertical mai divide"""
+
+import numpy as np
+arr= np.array([1,2,3,4,5,6,7,8])
+print(np.split(arr,2))
