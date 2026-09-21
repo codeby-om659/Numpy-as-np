5d717cc3bb9e527223581e434fe2b6c89d30c6d0
Om verma <sureshverma67895@gmail.com>
2026-09-21 07:06:20 +0530

    Create operators.py

---
+++ operators.py
@@ -0,0 +1,14 @@
+import numpy as np
+arr=np.array([1,2,3,4,5])
+print(arr+5)
+print(arr*2)
+print(arr**2)
+
+
+#Aggregation Function
+print(np.sum(arr))
+print(np.mean(arr))
+print(np.std(arr))
+print(np.var(arr))
+print(np.min(arr))
+print(np.max(arr))
