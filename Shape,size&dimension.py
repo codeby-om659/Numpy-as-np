b6ec5baf4d7c10320f94e74126699dac92389909
Om verma <sureshverma67895@gmail.com>
2026-09-21 06:59:10 +0530

    Create Shape, Size & Dimension demonstration script
    Added a script to demonstrate shape, size, and dimensions of numpy arrays.

---
+++ Shape,size&dimension.py
@@ -0,0 +1,16 @@
+# shape
+import numpy as np
+arr_2d=np.array([[1,2,3],
+                 [4,5,6]])
+print(arr_2d.shape)
+print(arr_2d.size)
+#size
+
+arr=np.array([1,2,3,1,2,3,4])
+print(arr.size)
+
+#ndim=how many dimension
+arr_1d=np.array([1,2,3])
+arr_2d=np.array([[1,2,3],[4,5,6]])
+print(arr_1d.ndim)
+print(arr_2d.ndim)
